dc14f2b158be987fe26307b7ea3b0499d03c6862
Ramon Gonzalez <ramongonzalez101@gmail.com>
2024-10-22 00:10:37 -0400

    creacion register

---
+++ front/src/app/Register/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Register from '../components/Register/register'
+
+
+const pageRegister = () => {
+  return (
+    <Register/>
+  )
+}
+
+export default pageRegister
