5472ae0f8bfdb851233f2f24fdddd221068fe87b
Ramon Gonzalez <ramongonzalez101@gmail.com>
2024-10-22 21:55:46 -0400

    primer UI register

---
+++ front/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '@/app/globals.css'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div>
+    <h1>Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
