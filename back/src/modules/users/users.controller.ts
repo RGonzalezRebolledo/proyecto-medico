36070275e96dcfd206fb9cded545a0b17bb56207
XsporosX@gmail.com <saul_751@hotmail.com>
2024-10-25 20:43:40 -0400

    ultimos cambios

--- back/src/modules/users/users.controller.ts
+++ back/src/modules/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Body, Param, Delete, Put, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Body, Param, Delete, Put, ParseUUIDPipe, HttpCode, HttpStatus } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 
@@ -7,21 +7,25 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.findOne(id);
   }
 
   @Put(':id')
+  @HttpCode(HttpStatus.OK)
   update(@Param('id', ParseUUIDPipe) id: string, @Body() updateUser: UpdateUserDto) {
     return this.usersService.update(id, updateUser);
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.OK)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.remove(id);
   }
