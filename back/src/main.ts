de57ed7cac4322188d48fd260bfb4ba295c334ad
XsporosX@gmail.com <saul_751@hotmail.com>
2024-11-10 10:37:45 -0400

    modificaciones

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 require('dotenv').config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  // app.enableCors({
+  //   origin: 'http://localhost:3000',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  //   allowedHeaders: 'Content-Type, Accept, Authorization',
+  // });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
